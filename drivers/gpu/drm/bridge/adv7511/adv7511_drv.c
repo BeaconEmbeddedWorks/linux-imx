723d63512180ae967790e475e87548af46fb790d
Adam Ford <aford@beaconembedded.com>
2023-07-10 08:57:47 -0500

    drm/bridge: adv7511: Enable shared interrupts
    On the imx8mp, the ADV7535 shares the interrupt with several
    GPIO expanders.  The GPIO expanders allow for shared interrupts,
    but the ADV expects the GPIO to itself, so it failes to initialize
    the hot-plug detect.  Enable IRQF_SHARED to allow IRQ sharing to
    facilitate hot-plug-detection

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1380,7 +1380,7 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 
 		ret = devm_request_threaded_irq(dev, i2c->irq, NULL,
 						adv7511_irq_handler,
-						IRQF_ONESHOT, dev_name(dev),
+						IRQF_ONESHOT | IRQF_SHARED, dev_name(dev),
 						adv7511);
 		if (ret)
 			goto err_unregister_cec;
